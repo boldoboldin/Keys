e7799b379cce474de5e2df5ea544680e747128b5
Luiz Guilherme de Souza Gonçalves <103260099+Luizgi@users.noreply.github.com>
2023-04-25 19:00:39 -0300

    Menu Hud
    Add New Game, Load Game working!

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+
+    public int ActualPhase;
+
+    private void Start()
+    {
+        if(ActualPhase > 0 && ActualPhase < 4)
+        {
+            PlayerPrefs.SetInt("FASE", ActualPhase);
+        }
+    }
+
+    public void Phase (int numberPhase)
+    {
+        SceneManager.LoadScene(numberPhase);
+    }
+
+    public void PassPhase()
+    {
+        int pPhase = ActualPhase + 1;
+        SceneManager.LoadScene(pPhase);
+    }
+
+    public void GoToSavedPhase()
+    {
+        int fSave = PlayerPrefs.GetInt("FASE");
+        SceneManager.LoadScene(fSave);
+    }
+
+}
